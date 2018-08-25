cc1b4c52c7c7f0e5545bcb6512675b8f76233f08
Michael Di Giacomi <digitaladrenalin@gmail.com>
2018-08-25 06:47:37 -0400

    Added SwaggerUI assigned it to default route

--- virshlib.api/Program.cs
+++ virshlib.api/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+using Swashbuckle.AspNetCore.Swagger;
+
 namespace virshlib.api
 {
     public class Program
--- virshlib.api/Startup.cs
+++ virshlib.api/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
+using Swashbuckle.AspNetCore.Swagger;
+
 namespace virshlib.api
 {
     public class Startup
@@ -25,6 +27,11 @@ namespace virshlib.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Contacts API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +41,17 @@ namespace virshlib.api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = "";
+                
+            });
 
             app.UseMvc();
         }
