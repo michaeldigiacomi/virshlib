118f98579f607b318d6eec5287b5e6a2ba0f7f85
Michael Di Giacomi <digitaladrenalin@gmail.com>
2018-08-30 13:20:32 -0400

    Putting in better checks

--- virshlib/Parsers/Domain.cs
+++ virshlib/Parsers/Domain.cs
@@ -21,17 +21,15 @@ namespace virshlib.Parsers
                     string domainText = commandTextSplit[i];
                     string[] domainValues = System.Text.RegularExpressions.Regex.Split(domainText, @"\s{2,}");
 
-                    DomainModel model = new DomainModel();
-                    try
+                    if (domainValues[2] == null)
                     {
+                        DomainModel model = new DomainModel();
                         model.ID = domainValues[0].Trim();
                         model.Name = domainValues[1].Trim();
                         model.Status = domainValues[2].Trim();
+
+                        DomainList.Add(model);
                     }
-                    catch (Exception err) {
-                        //Todo: Implement Logging Properly of failes parsing
-                    }
-                    DomainList.Add(model);
                 }
                 catch (Exception error)
                 {
