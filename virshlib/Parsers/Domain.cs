5b6b5828c1a2e0c86256e1f505d9d8c2a4bf5451
Michael Di Giacomi <digitaladrenalin@gmail.com>
2018-08-30 13:13:31 -0400

    Updated test case and error catching on domain controller

--- virshlib/Parsers/Domain.cs
+++ virshlib/Parsers/Domain.cs
@@ -16,16 +16,27 @@ namespace virshlib.Parsers
 
             for(int i= 2; i<commandTextSplit.Length; i++)
             {
-                string domainText = commandTextSplit[i];
-                string[] domainValues = System.Text.RegularExpressions.Regex.Split(domainText, @"\s{2,}");
+                try
+                {
+                    string domainText = commandTextSplit[i];
+                    string[] domainValues = System.Text.RegularExpressions.Regex.Split(domainText, @"\s{2,}");
 
-                DomainModel model = new DomainModel();
-
-                model.ID = domainValues[0].Trim();
-                model.Name = domainValues[1].Trim();
-                model.Status = domainValues[2].Trim();
-
-                DomainList.Add(model);
+                    DomainModel model = new DomainModel();
+                    try
+                    {
+                        model.ID = domainValues[0].Trim();
+                        model.Name = domainValues[1].Trim();
+                        model.Status = domainValues[2].Trim();
+                    }
+                    catch (Exception err) {
+                        //Todo: Implement Logging Properly of failes parsing
+                    }
+                    DomainList.Add(model);
+                }
+                catch (Exception error)
+                {
+                    throw;
+                }
             }
 
             return DomainList;
